f36ba42928c9cc12e487cc531dda475237ee62a9
Ming Li <mingrkli@gmail.com>
2025-02-24 15:36:34 -0800

    added the cursors in real time

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,10 +1,29 @@
 import { useEffect, useRef } from "react";
 import useWebSocket from "react-use-websocket";
 import throttle from "lodash.throttle";
+import { Cursor } from "./components/Cursor";
+
+const renderCursor = (users) => {
+    return Object.keys(users).map((uuid) => {
+        const user = users[uuid];
+
+        return <Cursor key={uuid} point={[user.state.x, user.state.y]} />;
+    });
+};
+
+const renderUsersList = (users) => {
+    return (
+        <ul>
+            {Object.keys(users).map((uuid) => {
+                return <li key={uuid}>{JSON.stringify(users[uuid])}</li>;
+            })}
+        </ul>
+    );
+};
 
 export function Home({ username }) {
     const WS_URL = "ws://localhost:8000";
-    const { sendJsonMessage } = useWebSocket(WS_URL, {
+    const { sendJsonMessage, lastJsonMessage } = useWebSocket(WS_URL, {
         queryParams: { username },
     });
 
@@ -17,6 +36,11 @@ export function Home({ username }) {
 
     // Run first time when the component is rendered
     useEffect(() => {
+        // ask the server to send everyone's state the second we load the components
+        sendJsonMessage({
+            x: 0,
+            y: 0,
+        });
         window.addEventListener("mousemove", (e) => {
             sendJsonMessageThrottled.current({
                 x: e.clientX,
@@ -25,5 +49,13 @@ export function Home({ username }) {
         });
     }, []);
 
+    if (lastJsonMessage) {
+        return (
+            <>
+                {renderCursor(lastJsonMessage)}
+                {renderUsersList(lastJsonMessage)}
+            </>
+        );
+    }
     return <h1>Hello, {username}</h1>;
 }
